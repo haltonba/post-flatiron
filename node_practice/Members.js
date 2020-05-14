a657bcb84e18548558b0c143e5eacbf9cf406025
haltonba <haltonba94@gmail.com>
2020-05-14 10:08:31 -0400

    First commit

---
+++ node_practice/Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: "Jeff",
+        email: "jeff@chegg.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Joe",
+        email: "joe@chegg.com",
+        status: "active"
+    },
+    {
+        id: 3,
+        name: "Angela",
+        email: "angela@chegg.com",
+        status: "active"
+    },
+    {
+        id: 4,
+        name: "Christina",
+        email: "christina@chegg.com",
+        status: "active"
+    }
+]
+
+module.exports = members
